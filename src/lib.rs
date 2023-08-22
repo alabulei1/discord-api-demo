033df0136de7ab1d3b4281adb673ef2962647825
Ji Chen <jaykchen@gmail.com>
2023-08-22 15:53:35 -0400

    v0.6 based

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,14 @@
-// use discord_flows::model::prelude::*;
 use discord_flows::{
-    http::HttpBuilder, model::application_command::CommandDataOptionValue, Bot, EventModel,
-    ProvidedBot,
+    application_command_handler,
+    http::HttpBuilder,
+    message_handler,
+    model::{
+        application::interaction::InteractionResponseType,
+        application_command::CommandDataOptionValue,
+        prelude::application::interaction::application_command::ApplicationCommandInteraction,
+        Message,
+    },
+    Bot, ProvidedBot,
 };
 use dotenv::dotenv;
 use flowsnet_platform_sdk::logger;
@@ -15,9 +22,11 @@ use std::env;
 
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
-pub async fn run() {
+pub async fn on_deploy() {
     dotenv().ok();
     logger::init();
+    let token = env::var("DISCORD_TOKEN").unwrap();
+    let bot = ProvidedBot::new(token);
     let discord_token = env::var("discord_token").unwrap();
     let commands_registered = env::var("COMMANDS_REGISTERED").unwrap_or("false".to_string());
 
@@ -28,46 +37,102 @@ pub async fn run() {
         }
         _ => {}
     }
-    let bot = ProvidedBot::new(discord_token);
-    bot.listen(|em| handle(&bot, em)).await;
+    // bot.listen(|em| handle(&bot, em)).await;
+
+    bot.listen_to_messages().await;
+    let channel_id = env::var("discord_channel_id").unwrap_or("channel_id not found".to_string());
+    let channel_id = channel_id.parse::<u64>().unwrap(); // Your channel id
+    bot.listen_to_application_commands_from_channel(channel_id)
+        .await;
 }
 
-async fn register_commands(discord_token: &str) {
-    let bot_id = env::var("bot_id").unwrap_or("1124137839601406013".to_string());
-    // let channel_id = env::var("discord_channel_id").unwrap_or("1128056246570860617".to_string());
-    //     let guild_id = env::var("discord_guild_id").unwrap_or("1128056245765558364".to_string());
-    //     // Define the Discord API endpoint for registering commands
-    //     let uri = format!(
-    //         "https://discord.com/api/v8/applications/{}/guilds/{}/commands",
-    //         bot_id, guild_id
-    //     );
+#[message_handler]
+async fn handle(msg: Message) {
+    let token = std::env::var("DISCORD_TOKEN").unwrap();
+    let bot = ProvidedBot::new(token);
+    let client = bot.get_client();
+    let channel_id = msg.channel_id;
+    let content = msg.content;
 
-    let command = serde_json::json!({
-        "name": "weather",
-        "description": "Get the weather for a city",
-        "options": [
-            {
-                "name": "city",
-                "description": "The city to lookup",
-                "type": 3,
-                "required": true
-            }
-        ]
-    });
+    if msg.author.bot {
+        return;
+    }
 
-    let http_client = HttpBuilder::new(discord_token)
-        .application_id(bot_id.parse().unwrap())
-        .build();
+    _ = client
+        .send_message(
+            channel_id.into(),
+            &serde_json::json!({
+                "content": content,
+            }),
+        )
+        .await;
+}
 
-    match http_client
-        .create_global_application_command(&command)
-        .await
-    {
-        Ok(_) => log::info!("Successfully registered command"),
-        Err(err) => log::error!("Error registering command: {}", err),
+#[application_command_handler]
+async fn handler(ac: ApplicationCommandInteraction) {
+    let token = std::env::var("DISCORD_TOKEN").unwrap();
+    let bot = ProvidedBot::new(token);
+    let client = bot.get_client();
+
+    _ = client
+        .create_interaction_response(
+            ac.id.into(),
+            &ac.token,
+            &serde_json::json!({
+                "type": InteractionResponseType::DeferredChannelMessageWithSource as u8,
+            }),
+        )
+        .await;
+    // tokio::time::sleep(Duration::from_secs(3)).await;
+    client.set_application_id(ac.application_id.into());
+    match ac.data.name.as_str() {
+        "weather" => {
+            let options = ac
+                .data
+                .options
+                .get(0)
+                .expect("Expected city option")
+                .resolved
+                .as_ref()
+                .expect("Expected city object");
+
+            let city = match options {
+                CommandDataOptionValue::String(s) => s,
+                _ => panic!("Expected string for city"),
+            };
+
+            let resp_inner = match get_weather(&city) {
+                Some(w) => format!(
+                    r#"Today: {},
+                Low temperature: {} °C,
+                High temperature: {} °C,
+                Wind Speed: {} km/h"#,
+                    w.weather
+                        .first()
+                        .unwrap_or(&Weather {
+                            main: "Unknown".to_string()
+                        })
+                        .main,
+                    w.main.temp_min as i32,
+                    w.main.temp_max as i32,
+                    w.wind.speed as i32
+                ),
+                None => String::from("No city or incorrect spelling"),
+            };
+            let resp = serde_json::json!(
+                {
+                    "content": resp_inner
+                }
+            );
+            _ = client
+                .edit_original_interaction_response(&ac.token, &resp)
+                .await;
+        }
+        _ => {}
     }
 }
-async fn handle<B: Bot>(bot: &B, em: EventModel) {
+
+/* async fn handle<B: Bot>(bot: &B, em: EventModel) {
     match em {
         EventModel::ApplicationCommand(ac) => {
             let client = bot.get_client();
@@ -75,54 +140,54 @@ async fn handle<B: Bot>(bot: &B, em: EventModel) {
             match ac.data.name.as_str() {
                 "weather" => {
                     let options = ac
-                        .data
-                        .options
-                        .get(0)
-                        .expect("Expected city option")
-                        .resolved
-                        .as_ref()
-                        .expect("Expected city object");
+                    .data
+                    .options
+                    .get(0)
+                    .expect("Expected city option")
+                    .resolved
+                    .as_ref()
+                    .expect("Expected city object");
 
-                    let city = match options {
-                        CommandDataOptionValue::String(s) => s,
-                        _ => panic!("Expected string for city"),
-                    };
+                let city = match options {
+                    CommandDataOptionValue::String(s) => s,
+                    _ => panic!("Expected string for city"),
+                };
 
-                    let resp_inner = match get_weather(&city) {
-                        Some(w) => format!(
-                            r#"Today: {},
-Low temperature: {} °C,
-High temperature: {} °C,
-Wind Speed: {} km/h"#,
-                            w.weather
-                                .first()
-                                .unwrap_or(&Weather {
-                                    main: "Unknown".to_string()
-                                })
-                                .main,
-                            w.main.temp_min as i32,
-                            w.main.temp_max as i32,
-                            w.wind.speed as i32
-                        ),
-                        None => String::from("No city or incorrect spelling"),
-                    };
-                    let resp = serde_json::json!(
-                        {
-                            "content": resp_inner
-                        }
-                    );
-                    _ = client.send_message(*channel_id, &resp).await;
-                }
-                _ => {}
+                let resp_inner = match get_weather(&city) {
+                    Some(w) => format!(
+                        r#"Today: {},
+                        Low temperature: {} °C,
+                        High temperature: {} °C,
+                        Wind Speed: {} km/h"#,
+                        w.weather
+                        .first()
+                        .unwrap_or(&Weather {
+                            main: "Unknown".to_string()
+                        })
+                        .main,
+                        w.main.temp_min as i32,
+                        w.main.temp_max as i32,
+                        w.wind.speed as i32
+                    ),
+                    None => String::from("No city or incorrect spelling"),
+                };
+                let resp = serde_json::json!(
+                    {
+                        "content": resp_inner
+                    }
+                );
+                _ = client.send_message(*channel_id, &resp).await;
             }
-        }
-        EventModel::Message(msg) => {
-            let client = bot.get_client();
-            let channel_id = msg.channel_id;
-            let content = msg.content;
+            _ => {}
         }
     }
+    EventModel::Message(msg) => {
+        let client = bot.get_client();
+        let channel_id = msg.channel_id;
+        let content = msg.content;
+    }
 }
+} */
 
 #[derive(Deserialize, Debug)]
 struct ApiResult {
@@ -173,4 +238,40 @@ fn get_weather(city: &str) -> Option<ApiResult> {
         }
     };
     None
+}
+
+async fn register_commands(discord_token: &str) {
+    let bot_id = env::var("bot_id").unwrap_or("1124137839601406013".to_string());
+    // let channel_id = env::var("discord_channel_id").unwrap_or("1128056246570860617".to_string());
+    //     let guild_id = env::var("discord_guild_id").unwrap_or("1128056245765558364".to_string());
+    //     // Define the Discord API endpoint for registering commands
+    //     let uri = format!(
+    //         "https://discord.com/api/v8/applications/{}/guilds/{}/commands",
+    //         bot_id, guild_id
+    //     );
+
+    let command = serde_json::json!({
+        "name": "weather",
+        "description": "Get the weather for a city",
+        "options": [
+            {
+                "name": "city",
+                "description": "The city to lookup",
+                "type": 3,
+                "required": true
+            }
+        ]
+    });
+
+    let http_client = HttpBuilder::new(discord_token)
+        .application_id(bot_id.parse().unwrap())
+        .build();
+
+    match http_client
+        .create_global_application_command(&command)
+        .await
+    {
+        Ok(_) => log::info!("Successfully registered command"),
+        Err(err) => log::error!("Error registering command: {}", err),
+    }
 }
